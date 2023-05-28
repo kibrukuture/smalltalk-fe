6062287917e16ecdfc744f3934a323e454a4f8e8
Kibru Kuture <kibrukuture@gmail.com>
2023-05-29 01:36:55 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import SmsField from './SmsField';
+
 function App() {
-  return <div className='bg-black text-[wheat] flex h-screen w-full  items-center justify-center'>Hello Vite</div>;
+  return (
+    <div className='bg-black text-[wheat] flex h-screen w-full  items-center justify-center'>
+      <SmsField />
+    </div>
+  );
 }
 
 export default App;
