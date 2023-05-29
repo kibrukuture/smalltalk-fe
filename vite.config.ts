cb68b0c0ab0ab8456c41497396a84f5837854341
Kibru Kuture <kibrukuture@gmail.com>
2023-05-29 21:33:59 +0300

    over secure websocket

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,3 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
 });
-
-/*
-# localhost
-
-VITE_LOCAL_HOST="localhost:8000"
-
-# remote host
-
-VITE_REMOTE_HOST="tolbel-express-server-production.up.railway.app"
-*/
