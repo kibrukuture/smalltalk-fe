c03cf8a7b1f6335b002f3d4be64487a4f6c8d451
Kibru Kuture <kibrukuture@gmail.com>
2023-05-29 15:58:36 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import Layout from './Layout';
+import Layout from './App';
 
 import './index.css';
 
