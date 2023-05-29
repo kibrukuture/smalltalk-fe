fd45e06f1ea4d096339536b3913ca58788091ed7
Kibru Kuture <kibrukuture@gmail.com>
2023-05-29 10:39:55 +0300

    .

---
+++ src/chat/page.tsx
@@ -0,0 +1,243 @@
+'use client';
+import { useEffect, useContext, useRef, useState } from 'react';
+import { ChatContext } from '../ChatContext';
+import ChatRoomContext, { RemotePeerVideoCallingStatus } from '../ChatRoomContext';
+import ChatBox from '@/components/ChatBox';
+import Bar from '@/components/Bar';
+import ConversationBox from '@/components/ConversationBox';
+import UserDetail from '@/components/UserDetail';
+import socket from '../socket.config';
+import Profile from '@/components/Profile';
+import Notificstion from '@/components/Notification';
+import Settings from '@/components/Settings';
+import { useRouter } from 'next/navigation';
+import { addNewMessage, deleteConversation } from '../util.fns';
+import { User } from '../ChatContext';
+import Calling from '@/components/Calling';
+//import Peer from 'peerjs';
+
+// select tab
+const tab: any = {
+  chat: <ConversationBox />,
+  profile: <Profile />,
+  notification: <Notificstion />,
+  setting: <Settings />,
+};
+
+export default function Chat() {
+  // local storage
+  const user = JSON.parse(localStorage.getItem('user') as string) as User;
+
+  // state
+  const [currentInnerWidth, setCurrentInnerWidth] = useState<number>(0);
+  const [remotePeerCalling, setRemotePeerCalling] = useState({} as RemotePeerVideoCallingStatus);
+  const [showVideoCallDisplayer, setShowVideoCallDisplayer] = useState(false);
+  const [showVoiceCallDisplayer, setShowVoiceCallDisplayer] = useState(false);
+  const [localUserVideoStream, setLocalUserVideoStream] = useState<MediaStream | null>(null);
+  const [remoteUserVideoStream, setRemoteUserVideoStream] = useState<MediaStream | null>(null);
+  const [caller, setCaller] = useState({} as User);
+  const [isCallAnswered, setIsCallAnswered] = useState(false);
+  const [localPeer, setLocalPeer] = useState(null);
+
+  //consume context
+  const { rooms, isChatRoomTapped, setRooms, currentOpenChatId, setCurrentOpenChatId, typing, setTyping, setIsAllChatsLoading, isAllChatsLoading, setUser, barCurrentTab, setFriendRequests, friendRequests, setIsUserNotAbleToSendFriendRequest } = useContext(ChatContext);
+
+  // router
+  const router = useRouter();
+
+  // ref
+  const notificationRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    // on reload set user data.
+    setUser(user);
+
+    // only run in browser.
+    // listen to window resize.
+    let onResize = () => {
+      setCurrentInnerWidth(typeof window !== 'undefined' ? window.innerWidth : 0);
+    };
+    if (typeof window !== 'undefined') {
+      setCurrentInnerWidth(window.innerWidth); // set current inner width.
+
+      window.addEventListener('resize', onResize);
+    }
+    // load all chats.
+    setIsAllChatsLoading(true);
+    fetch(`http://localhost:4040/api/loadchats/${JSON.parse(localStorage.getItem('user') as string).userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        // "Authorization": `Bearer ${token}`
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsAllChatsLoading(false);
+        if (data.status === 'ok') {
+          inititateRoom(data, setRooms);
+        } else {
+        }
+      });
+
+    // fetch all friend request.
+    fetch(`http://localhost:4040/api/user/friend-requests/${JSON.parse(localStorage.getItem('user') as string).userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        // "Authorization": `Bearer ${token}`
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setFriendRequests(data.user); // users wanting requst.
+      });
+
+    // on socket connect
+    socket.on('connect', () => {
+      const token = localStorage.getItem('logInToken');
+      if (!token) return router.push('/');
+
+      const user = JSON.parse(localStorage.getItem('user') as string);
+
+      // authenticate user( emit)
+      socket.emit('AuthenticateUser', {
+        token,
+        user,
+      });
+
+      // set up user or redirect to login page.
+      socket.on('AuthenticateUser', (data) => {
+        if (data.status === 'ok') {
+          // user is authenticated & set up
+          socket.emit('SetupUser', data.user);
+        } else {
+          // user is not authenticated
+          router.push('/');
+        }
+      });
+
+      // when a user joins online
+      socket.on('JoinUserOnline', (data) => console.log('JoinUserOnline', data));
+
+      // listen for friend request
+      socket.on('FriendRequestAccepted', (data) => {
+        inititateRoom(data, setRooms);
+        setCurrentOpenChatId(data.roomId);
+      });
+
+      // listen for a new message
+      socket.on('ExchangeChatMessage', (data) => {
+        console.log('a new message...', data);
+        const { message, roomId } = data;
+        //update rooms with new message
+        addNewMessage(roomId, message, setRooms);
+      });
+
+      // user is online, push notification.
+      socket.on('PushFriendRequestNotification', (data) => {
+        setFriendRequests([...friendRequests, data]);
+        if (notificationRef.current) notificationRef.current.play();
+      });
+    });
+
+    // on socket disconnect try to reconnect
+    socket.on('disconnect', () => {
+      socket.connect();
+    });
+
+    // incoming video call
+    socket.on('IncomingCall', (data) => {
+      const { caller, roomId, friend, flag } = data;
+      // const peer = new Peer(user.userId!, { host: '/', port: 3001 });
+      // setLocalPeer(peer);
+      // setShowVideoCallDisplayer(true);
+
+      setRemotePeerCalling({
+        isCalling: true,
+        peer: caller,
+        roomId,
+        flag,
+      });
+    });
+
+    // video call rejected
+    socket.on('CallRejected', (data) => {
+      const { caller, roomId, friend } = data;
+
+      console.log('Video call rejected,', localUserVideoStream);
+      setShowVideoCallDisplayer(false);
+      // localUserVideoStream && localUserVideoStream.getTracks().forEach((track) => track.stop());
+      // setLocalUserVideoStream(undefined);
+
+      console.log('local video stream: ', localUserVideoStream && localUserVideoStream.getTracks());
+    });
+
+    socket.on('onDeleteConversation', (data) => {
+      const { messageId, roomId, deletedBy, friend } = data;
+      // roomId: string, messageId: string, setRooms: (rooms: Map<string, Room>) => void
+      console.log('onDeleteConversation', data);
+
+      deleteConversation(roomId, messageId, setRooms);
+    });
+
+    // remote peer video call ended
+    // socket.on('RemotePeerCallEnd', (data) => {
+    //   const { callEndedBy, roomId, peer } = data;
+
+    //   localPeer!.destroy(); // distory localPeer.
+    //   console.log(localPeer?.destroyed, 'local peer destroyed');
+    //   // setLocalPeer(null);
+    //   setShowVideoCallDisplayer(false);
+    // });
+
+    // console.log('In chat page: localPeer', localPeer);
+    return () => {
+      // socket.off('connect');
+      window.removeEventListener('resize', onResize);
+
+      // unmount socket listeners
+      // console.log('I am being unmounted');
+    };
+  }, []);
+
+  // console.log(':Component Rendered:', '# of rooms are: ', rooms.get(currentOpenChatId)?.messages.length);
+  if (!rooms.size && isAllChatsLoading) return <div className='h-screen w-full bg-black flex items-center justify-center text-skin-base text-2xl font-bold text-center  '>loading</div>;
+
+  // console.log(rooms);
+  return (
+    <ChatRoomContext.Provider value={{ remotePeerCalling, setRemotePeerCalling, showVideoCallDisplayer, setShowVideoCallDisplayer, localUserVideoStream, setLocalUserVideoStream, caller, setCaller, isCallAnswered, setIsCallAnswered, localPeer, setLocalPeer, showVoiceCallDisplayer, setShowVoiceCallDisplayer }}>
+      <div className='max-h-screen h-screen w-full flex flex-col-reverse md:flex md:flex-row'>
+        {/* medium and large screen */}
+        {currentInnerWidth > 768 && <Bar />}
+        {currentInnerWidth > 768 && <div className=' h-screen max-h-screen flex flex-col gap-xs bg-skin-muted md:min-w-[30%] lg:min-w-[20%]'>{tab[barCurrentTab]}</div>}
+        {currentInnerWidth > 768 && <ChatBox />}
+
+        {/* on smalll screen */}
+        {currentInnerWidth <= 768 && !isChatRoomTapped && <Bar />}
+        {currentInnerWidth <= 768 && !isChatRoomTapped && <div className=' h-screen max-h-screen flex flex-col gap-xs bg-skin-muted md:min-w-[30%] lg:min-w-[20%]'>{tab[barCurrentTab]}</div>}
+        {currentInnerWidth <= 768 && isChatRoomTapped && <ChatBox />}
+        {false && <UserDetail />}
+        <audio ref={notificationRef} src='/sound-effects/notification.wav' />
+
+        {/* remote peer is video-calling */}
+        {remotePeerCalling.isCalling && <Calling />}
+      </div>
+    </ChatRoomContext.Provider>
+  );
+}
+//
+function inititateRoom(data: any, setRooms: any) {
+  const map = new Map();
+  data.rooms.forEach((room: any) => {
+    map.set(room.roomId, {
+      roomId: room.roomId,
+      userOneId: room.userOneId,
+      userTwoId: room.userTwoId,
+      createdAt: room.createdAt,
+      friend: room.user,
+      messages: room.message,
+    });
+  });
+  setRooms(map);
+}
---
+++ src/layout.tsx
@@ -0,0 +1,251 @@
+'use client';
+import './globals.css';
+import { useEffect, useState, StrictMode } from 'react';
+import { useRouter } from 'next/navigation';
+import AlertMessage from '@/components/AlertMessage';
+import { ChatContext, LastSeen, User, Alert, UserProfile, Room, Error, SearchResult, Chats, AllChats, Typing, Tab, Theme, Connection } from '@/app/ChatContext';
+import { RiCloseFill } from 'react-icons/ri';
+import Head from 'next/head';
+
+type SetRoom = (rooms: Map<string, Room>) => Map<string, Room>;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  // all states
+  const [error, setError] = useState<Error>({
+      signIn: '',
+      signUp: '',
+    }), //error
+    [alert, setAlert] = useState<Alert>({
+      show: false,
+      message: '',
+      title: '',
+      type: 'error',
+    }), //alert
+    [currentOpenChatId, setCurrentOpenChatId] = useState<string>(''),
+    [chatFriends, setChatFriends] = useState([] as SearchResult[]),
+    [chats, setChats] = useState([] as Chats[]),
+    [allChats, setAllChats] = useState([] as AllChats[]),
+    [typing, setTyping] = useState([] as Typing[]),
+    [isAllChatsLoading, setIsAllChatsLoading] = useState(false),
+    [barCurrentTab, setBarCurrentTab] = useState<Tab>('chat'),
+    [wallpaper, setWallpaper] = useState(''),
+    [theme, setTheme] = useState('light' as Theme),
+    [gallery, setGallery] = useState([] as string[]),
+    [friendRequests, setFriendRequests] = useState([] as User[]),
+    [isUserNotAbleToSendFriendRequest, setIsUserNotAbleToSendFriendRequest] = useState(false),
+    [lastSeen, setLastSeen] = useState({} as LastSeen),
+    [userProfile, setUserProfile] = useState({ url: '' } as UserProfile),
+    [user, setUser] = useState({} as User),
+    [rooms, setRooms] = useState(new Map() as Map<string, Room>),
+    [isChatRoomTapped, setIsChatRoomTapped] = useState(false);
+
+  // router
+  const router = useRouter();
+
+  // useEffect(() => {
+  //   if (!allChats.length) return;
+  //   setTyping(
+  //     allChats
+  //       .map((chat) => chat.id)
+  //       .map((id) => ({
+  //         id,
+  //         typing: false,
+  //       })),
+  //   );
+  //   return () => setTyping([]); //empty typing array on unmount
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const stunServer = [
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
+        { urls: 'stun:stun.stunprotocol.org:3478' },
+        { urls: 'stun:stun.services.mozilla.com:3478' },
+        //more stun servers
+      ];
+      // Create a RTCPeerConnection object
+      const peerConnection = new RTCPeerConnection({ iceServers: stunServer });
+      peerConnection.addEventListener('icecandidate', (event) => console.log(event.candidate));
+
+      console.log(peerConnection);
+    }
+  }, []);
+
+  const onUserSignIn = async (usr: { email: string; password: string }) => {
+    const data = await handleFetch('http://localhost:4040/signin', 'POST', usr);
+
+    if (data.status === 'ok') {
+      console.log(data);
+      //set token
+      localStorage.setItem('logInToken', data.token);
+      localStorage.setItem('user', JSON.stringify(data.user));
+
+      setUser(data.user); // set user
+      setAlert({
+        show: true,
+        message: 'Welcome back!',
+        title: 'Logged In',
+        type: 'success',
+      });
+      // after 4 seconds, remove alert
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: '',
+          title: '',
+          type: '',
+        });
+      }, 10000);
+
+      setError({
+        signIn: '',
+        signUp: '',
+      });
+      // success sign in
+      router.push('/chat');
+    } else setError((prev) => ({ ...prev, signIn: data.message })); // could not sign in
+  };
+
+  const onUserSignUp = async (usr: User) => {
+    //{ email: '', password: '', name: '', userName: '' }
+    const data = await handleFetch('http://localhost:4040/signup', 'POST', usr);
+    if (data.status === 'ok') {
+      setError({
+        signIn: '',
+        signUp: '',
+      });
+      // success alert
+      setAlert({
+        show: true,
+        message: 'You have successfully signed up!',
+        title: 'Success',
+        type: 'success',
+      });
+      // after 4 seconds, remove alert
+      setTimeout(() => {
+        setAlert({
+          show: false,
+          message: '',
+          title: '',
+          type: '',
+        });
+      }, 10000);
+      // success sign in
+      router.push('/');
+    } else setError((prev) => ({ ...prev, signUp: data.message })); // could not sign in
+  };
+
+  const onAlertClose = () => {
+    setAlert({
+      show: false,
+      message: '',
+      title: '',
+      type: 'error',
+    });
+  };
+
+  // console.log('all rooms: ', rooms, currentOpenChatId);
+  return (
+    <html lang='en'>
+      <Head>
+        <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+        <title>smalltalk</title>
+      </Head>
+
+      <body className=' bg-skin-fill  text-skin-base font-sans  '>
+        <ChatContext.Provider
+          value={{
+            onUserSignIn,
+            onUserSignUp,
+            setError,
+            setCurrentOpenChatId,
+            setChatFriends,
+            setChats,
+            setAllChats,
+            setTyping,
+            setIsAllChatsLoading,
+            setBarCurrentTab,
+            setWallpaper,
+            setTheme,
+            setGallery,
+            setFriendRequests,
+            setIsUserNotAbleToSendFriendRequest,
+            setLastSeen,
+            setUserProfile,
+            setUser,
+            setAlert,
+            setRooms,
+            setIsChatRoomTapped,
+            isChatRoomTapped,
+            rooms,
+            alert,
+            user,
+            userProfile,
+            lastSeen,
+            isUserNotAbleToSendFriendRequest,
+            friendRequests,
+            wallpaper,
+            theme,
+            gallery,
+            barCurrentTab,
+            isAllChatsLoading,
+            typing,
+            allChats,
+            chatFriends,
+            chats,
+            error,
+            currentOpenChatId,
+          }}
+        >
+          {children}
+          {alert.show && <AlertMessage alert={alert} onAlertClose={onAlertClose} />}
+          {isUserNotAbleToSendFriendRequest && (
+            <div className='font-mono fixed p-lg w-fit bottom-2 right-2 z-50 bg-red-200 text-red-400 rounded'>
+              <div className='h-full w-2 bg-red-500 '></div>
+              <div className='text-sm p-lg'>
+                <p>Error</p>
+                <p>You can not send friend request to this person.</p>
+              </div>
+              <button onClick={() => setIsUserNotAbleToSendFriendRequest(false)} className='p-lg absolute top-2 right-2 text-white'>
+                <RiCloseFill />
+              </button>
+            </div>
+          )}
+        </ChatContext.Provider>
+      </body>
+    </html>
+  );
+}
+
+const handleFetch = async (
+  url: string,
+  method: string,
+  body: {
+    email: string;
+    password: string;
+  },
+) => {
+  const res = await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+
+  return data;
+};
