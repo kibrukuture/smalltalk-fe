ca0a155725b02d844a82b3465f89514ff490783c
Kibru Kuture <kibrukuture@gmail.com>
2023-05-30 18:43:40 +0300

    .

--- src/socket.config.ts
+++ src/socket.config.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 // import { hostedAt } from './util.fns';
-socket = io('wss://tolbel-express-server-production.up.railway.app', { transports: ['websocket'] });
+const socket = io('wss://tolbel-express-server-production.up.railway.app', { transports: ['websocket'] });
 //const socket = io('ws://localhost:8000', { transports: ['websocket'] }); // local
 export default socket;
